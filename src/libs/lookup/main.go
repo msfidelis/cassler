d5d2a3a260af26aa36b7189b392a7126d3bf56cd
Matheus Scarpato Fidelis <matheus@nanoshots.com.br>
2020-12-07 19:06:40 -0300

    go channels for dns resolutions

--- src/libs/lookup/main.go
+++ src/libs/lookup/main.go
@@ -8,16 +8,29 @@ import (
 )
 
 func Lookup(url string, dns_server string) []string {
-	r := &net.Resolver{
-		PreferGo: true,
-		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-			d := net.Dialer{
-				Timeout: time.Millisecond * time.Duration(10000),
-			}
-			return d.DialContext(ctx, "udp", fmt.Sprintf("%s:%s", dns_server, "53"))
-		},
+	timer := time.NewTimer(1000000000)
+	ch := make(chan []string, 1)
+	go func() {
+		r := &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				d := net.Dialer{
+					Timeout: time.Millisecond * time.Duration(10000),
+				}
+				return d.DialContext(ctx, "udp", fmt.Sprintf("%s:%s", dns_server, "53"))
+			},
+		}
+		ip, err := r.LookupHost(context.Background(), url)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+		ch <- ip
+	}()
+	select {
+	case ips := <-ch:
+		return ips
+	case <-timer.C:
+		fmt.Printf("timeout resolving %s\n", url)
 	}
-	ip, _ := r.LookupHost(context.Background(), url)
-
-	return ip
+	return make([]string, 0)
 }
